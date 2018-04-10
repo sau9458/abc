4f49a7606782082de0691414ed9bdc0b13ecdebc
reactexcel <arun.etech2011@gmail.com>
2018-04-10 12:44:23 +0530

    first task ui

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      back_color_data: true,
-      back_color_weather: true
+      classData: "cell",
+      classWeather: "cell"
     };
     this.changeColorData = this.changeColorData.bind(this);
     this.changeColorWeather = this.changeColorWeather.bind(this);
   }
   changeColorData() {
     this.setState({
-      back_color_data: false,
-      back_color_weather: true,
+      classData: "Data",
+      classWeather: "cell"
     });
+    this.props.history.push("/DataAnalysis");
   }
   changeColorWeather() {
     this.setState({
-      back_color_data: true,
-      back_color_weather: false,
+      classData: "cell",
+      classWeather: "Weather"
     });
+    this.props.history.push("/weatherAnalysis");
   }
   render() {
-    let bgColorData = this.state.back_color_data ? "white" : "#E7EBFF";
-    let bgColorWeather = this.state.back_color_weather ? "white" : "#E7EBFF";
+    console.log(this.props);
+
     return (
-      <div className="App">
-        <div className="container">
-          <div
-            style={{ backgroundColor: bgColorData }}
-            onClick={this.changeColorData}
-          >
-            <p className="Data">Data Analysis</p>
-          </div>
-          <div
-            style={{ backgroundColor: bgColorWeather }}
-            onClick={this.changeColorWeather}
-          >
-            <p className="Weather">Today Weather</p>
-          </div>
+      <div className="container" style={{ float: "left" }}>
+        <div className="row" onClick={this.changeColorData}>
+          <p className={this.state.classData}>Data Analysis</p>
+        </div>
+        <div className="row" onClick={this.changeColorWeather}>
+          <p className={this.state.classWeather}>Today Weather</p>
         </div>
       </div>
     );
